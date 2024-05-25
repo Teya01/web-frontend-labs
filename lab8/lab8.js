84cd76afea1effb2a559103637cde48cdab93f84
Adalinskaya Teya <TAdalinskaya@yandex.ru>
2024-05-25 11:37:34 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -23,3 +23,29 @@ function showDate() {
         </div>
         `;
 }
+
+function showDateDetails() {
+    let out = document.getElementById('date-details');
+    let today = new Date();
+
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1; // Месяцы в JavaScript нумеруются от 0 до 11
+    let date = today.getDate();
+    let dayOfWeek = today.getDay(); // Возвращает день недели (0 - воскресенье, 1 - понедельник, ..., 6 - суббота)
+
+    let daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    out.innerHTML = `
+        <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; margin-bottom: 10px;">
+            <span style="font-weight: bold;">Текущий год:</span> ${year}
+        </div>
+        <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; margin-bottom: 10px;">
+            <span style="font-weight: bold;">Текущий месяц:</span> ${month}
+        </div>
+        <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; margin-bottom: 10px;">
+            <span style="font-weight: bold;">Текущая дата:</span> ${date}
+        </div>
+        <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; margin-bottom: 10px;">
+            <span style="font-weight: bold;">Текущий день недели:</span> ${daysOfWeek[dayOfWeek]}
+        </div>
+    `;
+}
